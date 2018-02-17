33a6e7d3011ecee949346ad6aee5e680241b4be6
Emin <eminvergili96@gmail.com>
2018-02-17 19:10:27 +0300

    minor bug fix and create-user added and code ordered

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,15 @@
 var User = require('../models/user');
 
 exports.user_info = function(req,res) {
-  User.findOne({ username: req.params.username }, function(error, result) {
+  console.log(req.body.username);
+  User.findOne({ username: req.body.username }, function(error, result) {
     if(error)(res.redirect("/"));
     res.json({ "username": result.username, "credit": result.credit, "session": result.session, "password": result.password });
   });
 };
 
 exports.session_controller = function(req, res, next) {
+  console.log(req.body.username);
   User.findOne({ username: req.body.username }, function(error, result) {
     if (error) { return next(error) };
     if (req.body.changedTo) {
@@ -43,4 +45,22 @@ exports.session_controller = function(req, res, next) {
       };
     };
   })
+};
+
+exports.create_user = function(req, res, next) {
+  User.findOne( { username: req.body.username }, function(error, result){
+    if(result){ res.json({ 'msg': 'username already taken' }); return res.end(); }
+    else {
+      var usr = new User({
+        username: req.body.username,
+        password: req.body.password,
+        credit: 11,
+      });
+
+      usr.save(function(err){
+        if(err){ return next(err); }
+        else{ return res.json({ 'msg': 'user created successfully' }); }
+      });
+    }
+  });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 var user_controller = require('../controllers/userController');
 
+router.post('/info', user_controller.user_info);
 router.post('/sessionChange', user_controller.session_controller);
-router.get('/:username', user_controller.user_info);
+router.post('/create', user_controller.create_user);
 
 module.exports = router;

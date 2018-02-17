09c13779f11cd5bd35e7dc1693e672de2b8a77c0
Emin <eminvergili96@gmail.com>
2018-02-17 05:14:13 +0300

    some models and controllers were not added

--- controllers/powerstationController.js
+++ controllers/powerstationController.js
@@ -1,9 +1,23 @@
 var PowerStation = require('../models/powerstation');
 
-exports.find_near_ps = function(req, res){
+exports.find_near_pb = function(req, res){
   var lat = req.body.latitude;
   var lon = req.body.longitude;
   var range = req.body.range;
 
-  
+  PowerStation.find({ latitude: { $gte: (lat - range), $lte: (lat + range) }, available_pb_num: { $gte: 1 } }, function(error, result) {
+    console.log(result);
+    res.json({'result': result});
+  });
+};
+
+exports.find_near_cp = function(req,res) {
+  var lat = req.body.latitude;
+  var lon = req.body.longitude;
+  var range = req.body.range;
+
+  PowerStation.find({ latitude: { $gte: (lat - range), $lte: (lat + range) }, available_cp_num: { $gte: 1 } }, function(error, result) {
+    console.log(result);
+    res.json({'result': result});
+  });
 };

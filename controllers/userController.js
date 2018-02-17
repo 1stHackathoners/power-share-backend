c967978b87e8ddcddaac355259d003ea6ca5959d
Emin <eminvergili96@gmail.com>
2018-02-17 08:21:49 +0300

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,3 +6,10 @@ exports.user_info = function(req,res) {
     res.json({ "username": result.username, "credit": result.credit, "session": result.session, "password": result.password });
   });
 };
+
+exports.session_controller = function(req, res, next) {
+  User.findOne({ username: req.body.username }, function(error, result) {
+    if (error) { return next(error) };
+    if (req.body.changedTo ) {};
+  })
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var user_controller = require('../controllers/userController');
 
+router.post('/sessionChange', user_controller.session_controller);
 router.get('/:username', user_controller.user_info);
 
 module.exports = router;
